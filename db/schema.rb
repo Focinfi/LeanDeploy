73475c879218a050fb14995a3530f5f493c3f064
focinfi <focinfi@gmail.com>
2015-05-01 22:46:47 +0800

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150501012151) do
 
   create_table "html_descs", force: :cascade do |t|
