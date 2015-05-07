3c8a72c19dd57b28fa03fb36a6f7c5d45845db78
focinfi <focinfi@gmail.com>
2015-05-07 11:52:10 +0800

    update places api

--- config/initializers/lina.rb
+++ config/initializers/lina.rb
@@ -4,14 +4,14 @@ Lina.setup do |config|
   #config.generate_views = true
 
   #是否开启入口参数校验
-  #config.params_check = true
+  config.params_check = false
 
   #入口参数校验时使用的 json schema 版本, 更多: http://tools.ietf.org/html/draft-zyp-json-schema-04
   #config.params_json_schema_version = :v4
 
 
   #是否开启返回值参数校验
-  #config.return_check = true
+  config.return_check = false
 
   #返回值校验时使用的 json schema 版本, 更多: http://tools.ietf.org/html/draft-zyp-json-schema-04
   #config.return_json_schema_version = :v4
