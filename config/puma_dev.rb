ac4a49d42e8064a5517d2cac72d41bc07b53ac85
focinfi <focinfi@gmail.com>
2015-05-13 18:04:39 +0800

    fix bug

--- config/puma_dev.rb
+++ config/puma_dev.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env puma
 
-environment ENV['RAILS_ENV'] || 'production'
+environment ENV['RAILS_ENV'] || 'development'
 
 #daemonize true
 
-pidfile "/home/deploy/project/LearnDeploy/tmp/pids/puma.pid"
-stdout_redirect "/home/deploy/project/LearnDeploy/tmp/log/stdout", "/home/deploy/project/LearnDeploy/tmp/log/stderr"
+pidfile "/home/deploy/project/LeanDeploy/tmp/pids/puma.pid"
+stdout_redirect "/home/deploy/project/LeanDeploy/tmp/log/stdout", "/home/deploy/project/LeanDeploy/tmp/log/stderr"
 
 threads 0, 16
 
